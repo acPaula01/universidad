2753697a940cd303802b3ad98063c5b3bb473936
Santiago Angel Mancera <77251122+santiagoangel04@users.noreply.github.com>
2024-04-03 18:34:06 -0500

    Update funcionApuntadores.cpp

--- codigo C++/funcionApuntadores.cpp
+++ codigo C++/funcionApuntadores.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int numero = 10,*dir_numero;
-	dir_numero =&numero;
+	int numero = 10,*dir_numero; //crea el púntero y la variable donde se guarda la posicion en memoria
+	dir_numero =&numero; //guarda la direccion de memoria de numero 
 	cout<<"Programa patra apuntadores"<<"\n\n\n";
 	
 	cout<<"Numero: "<<numero<<endl;
